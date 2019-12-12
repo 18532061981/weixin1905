098421f38fda30f8dff14d05c067d8acfb4edb80
wzd <www3051037702@163.com>
2019-12-12 07:17:04 +0000

    sj

--- app/Http/Controllers/Wx/WeixinController.php
+++ app/Http/Controllers/Wx/WeixinController.php
@@ -39,7 +39,8 @@ class WeixinController extends Controller
     public function receiv(){
         $log_file = "wx.log";
     //将接收的数据记录到日志文件
-        $data =json_encode($_POST);
+        $xml = file_get_contents("php://input");
+        $data =data('Y-m-d H:i:s') . $xml;
         file_put_contents($log_file,$data,FILE_APPEND);
 
     }
