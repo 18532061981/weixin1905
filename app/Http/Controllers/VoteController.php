8009d6613359b2fe2c50f0690c7d9be098b97560
wzd <www3051037702@163.com>
2019-12-18 19:59:16 +0800

    redis

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Redis;
 class VoteController extends Controller
 {
     //微信投票
     public function index(){
 
-        echo '<pre>';print_r($_GET);echo '</pre>';
+//        echo '<pre>';print_r($_GET);echo '</pre>';
         $code = $_GET['code'];
 
         //获取access_token
