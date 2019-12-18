e960d26170292f64b064b36ef444b021ebe55baf
wzd <www3051037702@163.com>
2019-12-18 19:53:09 +0800

    是否已投票

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -16,6 +16,26 @@ class VoteController extends Controller
         $data = $this->getAccessToken($code);
         //获取用户信息
         $user_info = $this->getUserInfo($data['access_token'],$data['openid']);
+
+        //处理业务逻辑
+
+        $openid = $user_info['openid'];
+        $key = 'ss:vote:wzd';
+        //判断是否已经投过票
+        if(Redis::zrank($key,$user_info['openid'])){
+            echo "已经投过票了";
+        }else{
+            Redis::Zadd($key,time(),$openid);
+        }
+        $total = Redis::zCard($key);        // 获取总数
+        echo '投票总人数： '.$total;echo '</br>';
+        $members = Redis::zRange($key,0,-1,true);       // 获取所有投票人的openid
+        echo '<pre>';print_r($members);echo '</pre>';
+        foreach($members as $k=>$v){
+            echo "用户： ".$k . ' 投票时间: '. date('Y-m-d H:i:s',$v);echo '</br>';
+        }
+
+
     }
 
     /**
@@ -38,8 +58,13 @@ class VoteController extends Controller
 
         $url='https://api.weixin.qq.com/sns/userinfo?access_token='.$access_token.'&openid='.$openid.'&lang=zh_CN';
         $json_data = file_get_contents($url);
-        $user_info = json_decode($json_data,true);
-        echo '<pre>';print_r($user_info);echo '</pre>';
+        $data = json_decode($json_data,true);
+        if(isset($data['errcode'])){
+            // TODO 错误处理
+            die("出错了 40001");  // 40001 获取用户信息标识失败
+
+        }
+        return $data;   //返回
     }
 
 }
